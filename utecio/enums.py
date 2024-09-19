ea76fb25056259734e91a77d2864c501411485d3
KittDoesntCode <50064397+KittDoesntCode@users.noreply.github.com>
2024-09-19 11:34:25 -0400

    Add Update U-Bolt-Pro

--- utecio/enums.py
+++ utecio/enums.py
@@ -8,6 +8,7 @@ class DeviceLockModel(Enum):
     BoltNFC = "Bolt-NFC"
     LEVER = "LEVER"
     UBolt = "U-Bolt"
+    UBoltPro = "U-Bolt-Pro"
     UBoltWiFi = "U-Bolt-WiFi"
     UBoltZWave = "U-Bolt-ZWave"
     UL3 = "SmartLockByBle"
